522183c562b5dcc143cdecaebd17b8b7125da185
周尚礼 <105686154+zsl2021533013@users.noreply.github.com>
2024-07-02 18:45:58 +0800

    Init

---
+++ Assets/GameMain/Scripts/Utility/PathManager.cs
@@ -0,0 +1,15 @@
+﻿namespace GameMain.Scripts.Utility
+{
+    public static class PathManager
+    {
+        public static string GetSceneAsset(string assetName)
+        {
+            return $"Assets/GameMain/Scenes/{assetName}.unity";
+        }
+
+        public static string GetUIAsset(string assetName)
+        {
+            return $"UI/{assetName}";
+        }
+    }
+}
